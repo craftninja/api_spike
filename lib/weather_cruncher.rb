0e29476eecd24271534bfb0d582651d03736c7f9
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-05 15:09:04 -0600

    User can get the 7 day forcast. Format could use some help

--- lib/weather_cruncher.rb
+++ lib/weather_cruncher.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'json'
+require 'time'
 
 class WeatherCruncher
 
@@ -20,10 +21,31 @@ class WeatherCruncher
     sky_condition = data['weather'].first['main']
     wind_speed = data['wind']['speed']
     current_weather = "Current weather in #{city}: "
-    current_weather += "skies are #{sky_condition.downcase}, "
-    current_weather += "current temperature is #{current_temp}, "
-    current_weather += "wind speed is #{wind_speed}. "
+    current_weather += "atmospheric condition: #{sky_condition.downcase}, "
+    current_weather += "current temperature: #{current_temp}, "
+    current_weather += "wind speed: #{wind_speed}. "
     current_weather += "Expect today's temperatures to range from #{min_temp} to #{max_temp}"
+  end
+
+  def forcast_7_day(city)
+    forcast_7_day = ''
+    response = @weather_connection.get "http://api.openweathermap.org/data/2.5/forecast/daily?q=#{city}&units=imperial&cnt=7"
+    seven_day_data = JSON.parse(response.body)
+    seven_day_data['list'].each do |day_data|
+      day = Time.at(day_data['dt']).strftime('%A')
+      day_temp = day_data['temp']['day']
+      max_temp = day_data['temp']['min']
+      min_temp = day_data['temp']['max']
+      sky_condition = day_data['weather'].first['main']
+      wind_speed = day_data['speed']
+      current_weather = "#{day} weather in #{city}: "
+      current_weather += "atmospheric condition: #{sky_condition.downcase}, "
+      current_weather += "day temperature: #{day_temp}, "
+      current_weather += "wind speed: #{wind_speed}. "
+      current_weather += "Expect #{day}'s temperatures to range from #{min_temp} to #{max_temp}."
+      forcast_7_day += current_weather
+    end
+    forcast_7_day
   end
 
 end
