83f504638375dd2a2219b25c4c7191f97ac61753
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-06 23:05:32 -0600

    User can list unstarted stories for a specific tracker project

--- lib/pivotal_tracker_cruncher.rb
+++ lib/pivotal_tracker_cruncher.rb
@@ -25,4 +25,20 @@ class PivotalTrackerCruncher
 
   end
 
+  def list_unstarted_stories(project_id)
+    conn = Faraday.new(:url => 'https://www.pivotaltracker.com') do |faraday|
+      faraday.request :url_encoded # form-encode POST params
+      faraday.response :logger # log requests to STDOUT
+      faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+    end
+    response = conn.get do |req|
+      req.url "/services/v5/projects/#{project_id}/stories?&with_state=unstarted"
+      req.headers['X-TrackerToken'] = @token
+    end
+    story_data = JSON.parse(response.body)
+    story_data.each do |story|
+      puts story['name']
+    end
+  end
+
 end
--- run_spike.rb
+++ run_spike.rb
@@ -4,3 +4,4 @@ require_relative 'lib/pivotal_tracker_cruncher'
 
 ptc = PivotalTrackerCruncher.new(ENV['TOKEN'])
 ptc.list_projects
+ptc.list_unstarted_stories(1029432)
