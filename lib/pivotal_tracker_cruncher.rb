7ca2eaa11af2bb49c23e0089dd93852f74ae3608
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-06 23:17:12 -0600

    User can list details of a particular tracker story

--- lib/pivotal_tracker_cruncher.rb
+++ lib/pivotal_tracker_cruncher.rb
@@ -41,4 +41,20 @@ class PivotalTrackerCruncher
     end
   end
 
+  def list_details_of_story(project_id, story_id)
+    conn = Faraday.new(:url => 'https://www.pivotaltracker.com') do |faraday|
+      faraday.request :url_encoded # form-encode POST params
+      faraday.response :logger # log requests to STDOUT
+      faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+    end
+    response = conn.get do |req|
+      req.url "/services/v5/projects/#{project_id}/stories/#{story_id}"
+      req.headers['X-TrackerToken'] = @token
+    end
+    story = JSON.parse(response.body)
+    story.each_pair do |type, detail|
+      puts "#{type}: #{detail}"
+    end
+  end
+
 end
--- run_spike.rb
+++ run_spike.rb
@@ -5,3 +5,4 @@ require_relative 'lib/pivotal_tracker_cruncher'
 ptc = PivotalTrackerCruncher.new(ENV['TOKEN'])
 ptc.list_projects
 ptc.list_unstarted_stories(1029432)
+ptc.list_details_of_story(1029432, 70243804)
